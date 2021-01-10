681af24b452f99ceeb58950b3e3880a9b506dc40
xhevat ibraimi <xhevat.ibraimi@gmail.com>
2021-01-10 16:52:20 +0100

    removed server side rendering on homepage

--- PersonalWebsite.Web/Areas/Home/HomeController.cs
+++ PersonalWebsite.Web/Areas/Home/HomeController.cs
@@ -1,25 +1,21 @@
-﻿using System.Linq;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using PersonalWebsite.Web.EntityFramework;
 
 namespace PersonalWebsite.Web.Areas.Home
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly PersonalWebsiteDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger, PersonalWebsiteDbContext context)
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            _context = context;
         }
 
         [HttpGet("/")]
         public IActionResult Index()
         {
-            return View(Views.Home.HomeView, _context.Posts.ToList());
+            return View(Views.Home.HomeView);
         }
     }
 }
